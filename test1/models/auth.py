d5477bf6232d644f1c564570e58c510a21f4ec79
Ningod <ningod5949@126.com>
2019-05-31 16:13:20 +0800

    test1-alembic提交版本记录

---
+++ test1/models/auth.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from sqlalchemy import (Column, Integer, String, DateTime, ForeignKey)
+from models.db import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50))
+    createtime = Column(DateTime, default=datetime.now)
+    email = Column(String(50))
+
+
+    def __repr__(self):
+        return '<User:#{}-{}>'.format(self.id, self.name)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    image_url = Column(String(200))
+
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', backref='posts', uselist=False, cascade='all')
+
+    def __repr__(self):
+        return "<Post:#{}>".format(self.id)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all()
